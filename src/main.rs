1c0cdc0873e772fb5e15c7d11abf7c200633fdaf
Justin Lambert <justin.lambert@luxgroup.com>
2018-07-31 17:06:09 +1000

    better display

--- src/main.rs
+++ src/main.rs
@@ -40,22 +40,24 @@ fn hash_first_file(path: PathBuf) -> BoxResult<HashResult> {
   Ok(Blake2b::digest_reader(&mut buffer.as_ref())?)
 }
 
-fn generic_check<'a, I, T>(check_fn: fn(PathBuf) -> BoxResult<T>, paths: I) -> Arc<Mutex<HashSet<PathBuf>>>
+fn generic_check<'a, I, T>(check_fn: fn(PathBuf) -> BoxResult<T>, paths: I) -> (Arc<Mutex<HashSet<PathBuf>>>, Arc<Mutex<Vec<(PathBuf, PathBuf)>>>)
 where
     I: ParallelIterator<Item = &'a PathBuf>,
     T: Eq + Hash + Sync + Send
 {
   let def_dupes = Arc::new(Mutex::new(HashSet::new()));
   let file_hashes: Arc<Mutex<HashMap<T, PathBuf>>> = Arc::new(Mutex::new(HashMap::new()));
+  let out = Arc::new(Mutex::new(Vec::<(PathBuf, PathBuf)>::new()));
   paths.for_each(|current_path| {
     if let Ok(data) = check_fn(PathBuf::from(&current_path)) {
       if let Some(path) = file_hashes.lock().unwrap().insert(data, current_path.clone()) {
         def_dupes.lock().unwrap().insert(current_path.clone());
         def_dupes.lock().unwrap().insert(path.to_path_buf());
+        out.lock().unwrap().push((current_path.clone(), path.to_path_buf()));
       }
     }
   });
-  def_dupes
+  (def_dupes, out)
 }
 
 fn is_hidden(entry: &DirEntry) -> bool {
@@ -76,14 +78,14 @@ fn main() {
     }
   });
   println!("Time since start was {}", now.elapsed().as_secs());
-  let paths = generic_check(byte_count_file, paths.lock().unwrap().par_iter());
+  let (paths, _) = generic_check(byte_count_file, paths.lock().unwrap().par_iter());
   println!("Time since start was {}", now.elapsed().as_secs());
-  let paths = generic_check(hash_first_file, paths.lock().unwrap().par_iter());
+  let (paths, _) = generic_check(hash_first_file, paths.lock().unwrap().par_iter());
   println!("Time since start was {}", now.elapsed().as_secs());
-  let paths = generic_check(hash_file, paths.lock().unwrap().par_iter());
+  let (_, out) = generic_check(hash_file, paths.lock().unwrap().par_iter());
   println!("Time since start was {}", now.elapsed().as_secs());
-  for dupe in paths.lock().unwrap().clone() {
-    println!("dupe: {}", dupe.display());
+  for (dupe1, dupe2) in out.lock().unwrap().clone() {
+    println!("dupe: {} | AND | {}", dupe1.display(), dupe2.display());
   }
   println!("Time since start was {}", now.elapsed().as_secs());
 
