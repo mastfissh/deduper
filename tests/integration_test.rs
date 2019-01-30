4f1c858b7e5538ed50919a8164b27990c05f1fd1
JL <mastfish@gmail.com>
2019-01-30 19:40:26 +1100

    add tests

---
+++ tests/integration_test.rs
@@ -0,0 +1,47 @@
+extern crate dupelib;
+
+use std::path::PathBuf;
+
+
+
+#[test]
+fn test_base_case() {
+    let options = dupelib::Opt{
+      debug: false,
+      paths: vec![],
+      minimum: None,
+      output: None,
+      timing: false,
+    };
+    assert_eq!(dupelib::detect_dupes(options), 0);
+}
+
+#[test]
+fn test_one_file() {
+    let path: PathBuf = [ "testdirs", "one_file"].iter().collect();
+    dbg!(path.clone().as_os_str());
+    let options = dupelib::Opt{
+      debug: false,
+      paths: vec![path],
+      minimum: None,
+      output: None,
+      timing: false,
+    };
+    assert_eq!(dupelib::detect_dupes(options), 0);
+}
+
+#[test]
+fn test_ident_files() {
+    let mut path = PathBuf::from(file!());
+    path.pop();    
+    path.push("testdirs");
+    path.push("two_ident_files");    
+    let options = dupelib::Opt{
+      debug: false,
+      paths: vec![path],
+      minimum: None,
+      output: None,
+      timing: false,
+    };
+    assert_eq!(dupelib::detect_dupes(options), 1);
+}
